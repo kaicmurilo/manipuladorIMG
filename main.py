8d39d3796ed9e66d51413ccc84d1f36cb675f87b
Kaic Nunes <kaic.nunes@MASCARENHAS.CORP>
2022-06-15 09:16:12 -0400

    input e salvar documento com nome da pessoa

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+
+
+nome = input('Nome:  ')
+cargo = input('Cargo: ')
+telefone = input('Telefone: ')
+
 img = Image.open("modelVazio.png")
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("Ubuntu-B.ttf", 60)
 font2 = ImageFont.truetype("Ubuntu-R.ttf", 30)
 font3 = ImageFont.truetype("Ubuntu-B.ttf", 40)
-draw.text((770, 50), "Kaic Nunes", (255, 255, 255), font=font)
-draw.text((805, 149), "Coord. Tecnologia", (255, 255, 255), font=font2)
-draw.text((893, 250), "3041-8889", (255, 255, 255), font=font3)
-img.save('sample.png')
+draw.text((770, 50), nome, (255, 255, 255), font=font)
+draw.text((805, 149), cargo, (255, 255, 255), font=font2)
+draw.text((893, 250), telefone, (255, 255, 255), font=font3)
+img.save(nome+'.png')
