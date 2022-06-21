8758ec5806c8d9005d503c2defa8c00bc48eedcb
Kaic Nunes <kaic.nunes@MASCARENHAS.CORP>
2022-06-21 18:07:35 -0400

    conversao para string

--- alterarAssPrompt.py
+++ alterarAssPrompt.py
@@ -1,8 +1,9 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+import base64
 
-#pip install  PyQt5
+# pip install  PyQt5
 
 # nome = input('Nome:  ')
 # cargo = 'Paralegal'
@@ -36,6 +37,22 @@ while n != 'n':
     draw.text((805, 149), cargo, (255, 255, 255), font=font2)
     draw.text((893, 250), telefone, (255, 255, 255), font=font3)
     img.save(nome+'.png')
+
+    # converter em string
+    with open(nome+'.png', "rb") as image2string:
+        converted_string = base64.b64encode(image2string.read())
+    # print(converted_string)
+    with open(nome+'.bin', "wb") as file:
+        file.write(converted_string)
+
+    # convertendo de string para imagem
+    file = open('encode.bin', 'rb')
+    byte = file.read()
+    file.close()
+
+    decodeit = open(nome+' convertido-de-string.png', 'wb')
+    decodeit.write(base64.b64decode((byte)))
+    decodeit.close()
 
     n = input('Para parar digite n ou ctrl + c: ')
 else:
